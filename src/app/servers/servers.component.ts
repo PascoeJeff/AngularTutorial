9627a0b596a1260dccfe7f6058a60189fab17b65
Jeff Pascoe <jpascoe@solstice.com>
2018-09-06 09:03:49 -0500

    An extended example of data binding.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,6 +12,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = "No status was created.";
+  serverName = "";
   
   constructor() { 
     setTimeout(() => {
@@ -23,7 +24,11 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = "Server was created.";
+    this.serverCreationStatus = this.serverName + " was created.";
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
