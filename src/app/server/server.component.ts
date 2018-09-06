6b8dda84ec69bae6e3f58c6175b68be64d1303d7
Jeff Pascoe <jpascoe@solstice.com>
2018-09-06 08:35:18 -0500

    An example of string interpolation.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,10 +2,14 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html',
+    templateUrl: './server.component.html'
     
-
 })
 export class ServerComponent {
+    serverId = 10;
+    serverStatus: string = 'offline';
 
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
